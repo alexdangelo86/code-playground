e017810ca390522b62b6a008688615764ee326f0
alexdangelo86 <alexdangelo86@gmail.com>
2022-11-01 23:04:15 +0000

    Read to do list from siteData.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import logo from './logo.svg'
 import './App.css'
 import { TodoList } from './Components/TodoList'
 import Sidebar from './Components/Sidebar'
+import { Box } from '@mui/material'
+
+const siteData = [
+  { name: 'Todo List' },
+  { name: 'Basic Form' }
+]
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar/>
+    <Box 
+      sx={{
+        display: 'flex',
+      }}
+    
+    >
+
+      <Sidebar items={siteData} />
       <TodoList/>
-    </div>
+    </Box>
   )
 }
 
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,56 +1,42 @@
 import { Box } from '@mui/system'
-import { Typography } from '@mui/material'
 import React from 'react'
-import box from '@mui/material/Box'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import Divider from '@mui/material/Divider'
 
-const Sidebar = () => {
+const Sidebar = ({ items }) => {
   return (
     <Box 
       sx={{ 
         width: '100%',
         maxWidth: 360,
-        bgcolor: '#03a9f4',
+        bgcolor: '#009688',
         color: 'white',
         height: '100vh'
       }}
     >
-      <Typography>
-      Code Playground
-      </Typography>
+  
       <nav aria-label="main mailbox folders">
         <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Inbox" />
-            </ListItemButton>
+          <ListItem>
+
+            <ListItemText primary= 'Code Playground' />
           </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Drafts" />
-            </ListItemButton>
-          </ListItem>
+
+          <Divider />
+
+          {items.map((item, index) => {
+            return (
+              <ListItem disablePadding key={index}>
+                <ListItemText primary={item.name} />
+              </ListItem>
+
+            )
+          })}
         </List>
       </nav>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Trash" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary="Spam" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
+      
     </Box>
 
   )
