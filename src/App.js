d1494c02e11fd8f8d14360af943273ca1df0c12f
alexdangelo86 <alexdangelo86@gmail.com>
2022-11-08 21:33:37 +0000

    Set up rounter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { TodoList } from './Components/TodoList'
 import Sidebar from './Components/Sidebar'
 import { Box } from '@mui/material'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const siteData = [
-  { name: 'Todo List' },
-  { name: 'Basic Form' }
+  { name: 'Todo List', route: '/todo-list' },
+  { name: 'Basic Form', route: '/basic-form' },
 ]
 
 function App() {
   return (
-    <Box 
-      sx={{
-        display: 'flex',
-      }}
-    
-    >
-
-      <Sidebar items={siteData} />
-      <TodoList/>
-    </Box>
+    <Router>
+      <Box
+        sx={{
+          display: 'flex',
+        }}
+      >
+        <Sidebar items={siteData} />
+        <Routes>
+          <Route path="/todo-list" element={<TodoList />} />
+        </Routes>
+      </Box>
+    </Router>
   )
 }
 
 export default App
-
-
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,43 +4,51 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import Divider from '@mui/material/Divider'
+import ListItemButton from '@mui/material/ListItemButton'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = ({ items }) => {
+  const navigate = useNavigate()
+
+  const handleItemClick = (route) => {
+    navigate(route)
+  }
+
   return (
-    <Box 
-      sx={{ 
+    <Box
+      sx={{
         width: '100%',
         maxWidth: 360,
         bgcolor: '#009688',
         color: 'white',
-        height: '100vh'
+        height: '100vh',
       }}
     >
-  
       <nav aria-label="main mailbox folders">
         <List>
           <ListItem>
-
-            <ListItemText primary= 'Code Playground' />
+            <ListItemText primary="Code Playground" />
           </ListItem>
 
           <Divider />
 
           {items.map((item, index) => {
             return (
-              <ListItem disablePadding key={index}>
-                <ListItemText primary={item.name} />
+              <ListItem
+                disablePadding
+                key={index}
+                onClick={() => handleItemClick(item.route)}
+              >
+                <ListItemButton>
+                  <ListItemText primary={item.name} />
+                </ListItemButton>
               </ListItem>
-
             )
           })}
         </List>
       </nav>
-      
     </Box>
-
   )
-
 }
 
 export default Sidebar
