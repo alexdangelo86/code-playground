e2822c33e3e2f1e2ca8b92cadba39a31fbc7094a
alexdangelo86 <alexdangelo86@gmail.com>
2022-11-15 21:41:11 +0000

    finish Basic form

--- src/Components/BasicForm.js
+++ src/Components/BasicForm.js
@@ -1,21 +1,36 @@
 import { Card, Box, ButtonBase, Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 const BasicFrom = () => {
+  const [name, setName ] = ('')
+  const [email, setEmail] = ('')
+  const [password, setPassword] = ('')
+  const [confirmPassword, setConfrimPassword] = useState ('')
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    window.alert(`name: ${name} email: ${email}`)
+  }
+
   return (
     <Card sx={{ padding: '20px' }}>
-      <form>
+      <form onSubmit={handleSubmit}>
+
         <Box sx={{ marginBottom: '15px' }}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" value="" />
+          <input type="text" name="name" id="name" value={name} onChange={(event) => setName(event.tarqet.value)} />
         </Box>
+
         <Box sx={{ marginBottom: '15px' }}>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" value="" />
+          <input 
+            type="text" 
+            name="email" 
+            id="email" 
+            value={email} onChange={(event) => setEmail(event.taget.value)}/>
         </Box>
         <Box sx={{ marginBottom: '15px' }}>
           <label htmlFor="password">password</label>
-          <input type="password" name="password" id="password" value="" />
+          <input type="password" name="password" id="password" value={password} onChange={(event) => setPassword(event.targe.value)}/>
         </Box>
         <Box sx={{ marginBottom: '15px' }}>
           <label htmlFor="confirm-password">Confirm Password</label>
@@ -23,7 +38,8 @@ const BasicFrom = () => {
             type="password"
             name="confirmPassword"
             id="confirm-password"
-            value=""
+            value={confirmPassword}
+            onChange={(event) => setConfrimPassword(event.target.value)} 
           />
         </Box>
         <button type="submit">Submit</button>
