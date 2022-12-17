39340b5b4584094508244dcba77a54da913b8a09
alexdangelo86 <alexdangelo86@gmail.com>
2022-12-17 15:50:14 +0000

    second step

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,11 +1,37 @@
 import { Box } from '@mui/system'
 import React from 'react'
 import image from '../Img/MIDNIGHT_TREAT_1.jpg'
+import { Button, Typography } from '@mui/material'
 
 const Images = () => {
   return (
-    <Box>
-      <img src={image} />
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        background: `url(${image})`,
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <Button sx={{ width: '50%' }}>Listen now</Button>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mb: 5,
+        }}
+      >
+        <Typography color="white" variant="h2" fontSize={70}>
+          MIDNIGHT TREAT
+        </Typography>
+        <Typography color="white" variant="h3">
+          Alex D&apos;Angelo & Stretch the Producer
+        </Typography>
+      </Box>
     </Box>
   )
 }
