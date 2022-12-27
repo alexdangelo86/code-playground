71a4a7f3f864bdc7e8de592c854b5fb104e02e17
alexdangelo86 <alexdangelo86@gmail.com>
2022-12-27 19:43:44 +0000

    Reducer

--- src/Components/xmas.js
+++ src/Components/xmas.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Box } from '@mui/system'
+import useSomethings from '../hooks/useSomething'
 
 const Xmas = () => {
+  const { open, setOpen, list, setList, userName, setUsername } = useSomethings
   return <div>Merry Christmas</div>
 }
 
